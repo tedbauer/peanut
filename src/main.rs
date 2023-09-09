c0465c44c29c91639d8f04bb03db5e844714f8e5
tedbauer <tjb272@cornell.edu>
2023-09-09 19:11:54 -0400

    Hack together basic CRUD.

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,140 @@
-#[macro_use] extern crate rocket;
+use axum::response::Html;
+use axum::response::IntoResponse;
+use axum::routing::get;
+use axum::routing::post;
+use axum::{
+    extract::{Query, State},
+    http::StatusCode,
+    response::Redirect,
+    Router,
+};
+use chrono::Local;
+use serde::{Deserialize, Serialize};
 
-use rocket::http::ContentType;
+use sqlite::Connection;
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::vec::Vec;
+use tera::Tera;
 
-#[get("/")]
-fn index() -> (ContentType, &'static str) {
-    (ContentType::HTML, r#"
-    <!DOCTYPE html>
-    <html>
-   <head>
-   <style>
-   .box {
-       border-style: solid;
-       padding: 25px;
-       width: 750px;
-       box-shadow: 5px 5px 0 0 black;
-       margin-top: 100px;
-       margin-left: 100px;
-   }
-   .menu {
-       border-style: solid;
-       padding: 25px;
-       width: 300px;
-       box-shadow: 5px 5px 0 0 black;
-       margin-left: 100px;
-       margin-top: 20px;
-       font-size: 25px;
-   }
-   </style>
-   </head>
-   <body>
-   <title>hello</title>
-   <div class="box">
-       <h1>Example card
-   
-           <span style="float:right; font-size: 20px"><a href="google.com">edit</a></span>
-   
-       </h1>
-       <h2 style="color: grey">09/09/2023</h2>
-       <p>
-       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-       </p>
-       <h3 style="color: grey">References</h3>
-       <ol>
-           <li>Reference 1</li>
-           <li>Reference 2</li>
-           <li>Reference 3</li>
-       </ol>
-   
-   </div>
-   
-   <div class="menu">
-       <a href="google.com">+ Write a new card</a>
-   </div>
-   </body>
-    </html>
-    "#)
+#[derive(Debug, Deserialize)]
+#[allow(dead_code)]
+struct Params {
+    title: String,
+    content: String,
+    date: String,
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+#[derive(Debug, Deserialize)]
+#[allow(dead_code)]
+struct NoteParams {
+    title: String,
+}
+
+#[derive(Debug, Deserialize)]
+#[allow(dead_code)]
+struct WriteCardParams {
+    date: String,
+}
+
+async fn note(
+    Query(params): Query<NoteParams>,
+    State(state): State<Arc<Mutex<Connection>>>,
+) -> impl IntoResponse {
+    let mut tera = Tera::new("templates/**/*").unwrap();
+    let mut context = tera::Context::new();
+
+    let query = format!("SELECT * FROM users WHERE title = '{}'", params.title);
+    let conn: Arc<Mutex<Connection>> = state.clone();
+    let conn2 = conn.lock().unwrap();
+
+    let content = conn2
+        .prepare(query)
+        .unwrap()
+        .into_iter()
+        .into_iter()
+        .map(|row| row.unwrap().read::<&str, _>("content").to_string())
+        .collect::<Vec<_>>()
+        .join("");
+
+    context.insert("content", &content);
+    context.insert("title", &params.title);
+    Html(tera.render("card.html.tera", &context).unwrap())
+}
+
+async fn hello(State(state): State<Arc<Mutex<Connection>>>) -> impl IntoResponse {
+    let mut tera = Tera::new("templates/**/*").unwrap();
+    let mut context = tera::Context::new();
+
+    let query = "SELECT * FROM users";
+    let conn: Arc<Mutex<Connection>> = state.clone();
+    let conn2 = conn.lock().unwrap();
+
+    let titles = conn2
+        .prepare(query)
+        .unwrap()
+        .into_iter()
+        .into_iter()
+        .map(|row| row.unwrap().read::<&str, _>("title").to_string())
+        .map(|title| format!("<li><a href=\"/note?title={}\">{}</a></li>", title, title))
+        .collect::<Vec<_>>()
+        .join("");
+
+    context.insert("notes", &titles);
+    Html(tera.render("index.html.tera", &context).unwrap())
+}
+
+async fn put_card(
+    Query(params): Query<Params>,
+    State(state): State<Arc<Mutex<Connection>>>,
+) -> impl IntoResponse {
+    let mut tera = Tera::new("templates/**/*").unwrap();
+    let mut context = tera::Context::new();
+
+    let query = format!(
+        "INSERT INTO users VALUES ('{}', '{}')",
+        params.title, params.content
+    );
+    let conn: Arc<Mutex<Connection>> = state.clone();
+    let conn2 = conn.lock().unwrap();
+    conn2.execute(query).unwrap();
+    Redirect::to("/")
+}
+
+async fn new_card(
+) -> impl IntoResponse {
+    let mut tera = Tera::new("templates/**/*").unwrap();
+    let mut context = tera::Context::new();
+    context.insert("date", &format!("{}", Local::now().format("%m/%d/%Y")));
+    Html(tera.render("write-card.html.tera", &context).unwrap())
+}
+
+#[tokio::main]
+async fn main() {
+    let connection = sqlite::open(":memory:").unwrap();
+
+    let query = "
+    CREATE TABLE IF NOT EXISTS users (title TEXT, content TEXT);
+";
+    connection.execute(query).unwrap();
+
+    println!("we are now here.");
+
+    let query = "SELECT * FROM users";
+
+    let conn = Mutex::new(connection);
+
+    let shared_state = Arc::new(conn);
+
+    let app = Router::new()
+        .route("/", get(hello))
+        .route("/write-a-card", get(new_card))
+        .route("/put-card", get(put_card))
+        .route("/note", get(note))
+        .with_state(shared_state);
+
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
