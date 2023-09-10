22ab0c892d725c0bb7f8597c865fb3bafb60d3fb
tedbauer <tjb272@cornell.edu>
2023-09-10 16:45:31 -0400

    Add login functionality.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use axum::response::Html;
 use axum::response::IntoResponse;
 use axum::routing::get;
 use axum::routing::post;
+use rand::Rng;
+
 use axum::{
     extract::{Query, State},
     http::StatusCode,
@@ -9,6 +11,9 @@ use axum::{
     Router,
 };
 extern crate markdown;
+use axum_sessions::{
+    async_session::MemoryStore, extractors::WritableSession, PersistencePolicy, SessionLayer,
+};
 use chrono::Local;
 use serde::{Deserialize, Serialize};
 
@@ -18,12 +23,23 @@ use std::sync::Mutex;
 use std::vec::Vec;
 use tera::Tera;
 
+struct AppState {
+    connection: Mutex<Connection>,
+}
+
 #[derive(Debug, Deserialize)]
 #[allow(dead_code)]
 struct Params {
     title: String,
     content: String,
     date: String,
+}
+
+#[derive(Debug, Deserialize)]
+#[allow(dead_code)]
+struct LoginParams {
+    username: String,
+    password: String,
 }
 
 #[derive(Debug, Deserialize)]
@@ -40,16 +56,17 @@ struct WriteCardParams {
 
 async fn note(
     Query(params): Query<NoteParams>,
-    State(state): State<Arc<Mutex<Connection>>>,
+    State(state): State<Arc<AppState>>,
+    session: WritableSession,
 ) -> impl IntoResponse {
     let mut tera = Tera::new("templates/**/*").unwrap();
     let mut context = tera::Context::new();
 
-    let query = format!("SELECT * FROM users WHERE title = '{}'", params.title);
-    let query2 = format!("SELECT * FROM users WHERE title = '{}'", params.title);
-    let query3 = "SELECT * FROM users";
-    let conn: Arc<Mutex<Connection>> = state.clone();
-    let conn2 = conn.lock().unwrap();
+    let query = format!("SELECT * FROM notes WHERE title = '{}'", params.title);
+    let query2 = format!("SELECT * FROM notes WHERE title = '{}'", params.title);
+    let query3 = "SELECT * FROM notes";
+    let conn: Arc<AppState> = state.clone();
+    let conn2 = conn.connection.lock().unwrap();
 
     let content_md = conn2
         .prepare(query)
@@ -70,7 +87,7 @@ async fn note(
         .collect::<Vec<_>>()
         .join("");
 
-        let titles = conn2
+    let titles = conn2
         .prepare(query3)
         .unwrap()
         .into_iter()
@@ -80,6 +97,26 @@ async fn note(
         .collect::<Vec<_>>()
         .join("");
 
+        match &session.get::<String>("username") {
+            Some(val) => {
+                context.insert("username", &val);
+                context.insert("logged_in", &1);
+            }
+            None => {
+                context.insert("username", &"");
+                context.insert("logged_in", &0);
+            }
+        }
+    
+
+    // match &session.get::<usize>("foo") {
+    //     Some(val) => {
+    //         context.insert("content", &val);
+    //     }
+    //     None => {
+    //         context.insert("content", &content);
+    //     }
+    // }
     context.insert("content", &content);
     context.insert("title", &params.title);
     context.insert("notes", &titles);
@@ -87,14 +124,54 @@ async fn note(
     Html(tera.render("card.html.tera", &context).unwrap())
 }
 
-async fn hello(State(state): State<Arc<Mutex<Connection>>>) -> impl IntoResponse {
+async fn login(
+    State(state): State<Arc<AppState>>,
+    mut session: WritableSession,
+    Query(params): Query<LoginParams>,
+) -> impl IntoResponse {
+    let query = format!("SELECT * FROM users WHERE username = '{}'", params.username);
+
+    let conn: Arc<AppState> = state.clone();
+    let conn2 = conn.connection.lock().unwrap();
+
+    let pwd = conn2
+        .prepare(query)
+        .unwrap()
+        .into_iter()
+        .into_iter()
+        .map(|row| row.unwrap().read::<&str, _>("password").to_string())
+        .collect::<Vec<_>>()
+        .join("");
+
+    if params.password.eq(&pwd) {
+        println!("yep!");
+        session
+            .insert("username", params.username)
+            .expect("Could not store the answer.");
+    } else {
+        println!("nope");
+    }
+    Redirect::to(&("/"))
+}
+
+async fn logout(mut session: WritableSession) -> impl IntoResponse {
+    session
+        .insert("username", "")
+        .expect("Could not store the answer.");
+    Redirect::to(&("/"))
+}
+
+async fn hello(
+    State(state): State<Arc<AppState>>,
+    mut session: WritableSession,
+) -> impl IntoResponse {
     let mut tera = Tera::new("templates/**/*").unwrap();
     let mut context = tera::Context::new();
 
-    let query = "SELECT * FROM users";
-    let query2 = "SELECT * FROM users";
-    let conn: Arc<Mutex<Connection>> = state.clone();
-    let conn2 = conn.lock().unwrap();
+    let query = "SELECT * FROM notes";
+    let query2 = "SELECT * FROM notes";
+    let conn: Arc<AppState> = state.clone();
+    let conn2 = conn.connection.lock().unwrap();
 
     let titles = conn2
         .prepare(query)
@@ -116,6 +193,16 @@ async fn hello(State(state): State<Arc<Mutex<Connection>>>) -> impl IntoResponse
         .collect::<Vec<_>>()
         .join("<br>");
 
+    match &session.get::<String>("username") {
+        Some(val) => {
+            context.insert("username", &val);
+            context.insert("logged_in", &1);
+        }
+        None => {
+            context.insert("username", &"");
+            context.insert("logged_in", &0);
+        }
+    }
 
     context.insert("notes", &titles);
     context.insert("notes_drop", &titles2);
@@ -125,51 +212,65 @@ async fn hello(State(state): State<Arc<Mutex<Connection>>>) -> impl IntoResponse
 
 async fn put_card(
     Query(params): Query<Params>,
-    State(state): State<Arc<Mutex<Connection>>>,
+    State(state): State<Arc<AppState>>,
 ) -> impl IntoResponse {
     let mut tera = Tera::new("templates/**/*").unwrap();
     let mut context = tera::Context::new();
 
     let query = format!(
-        "INSERT INTO users VALUES ('{}', '{}', '{}')",
+        "INSERT INTO notes VALUES ('{}', '{}', '{}')",
         params.title, params.content, params.date
     );
-    let conn: Arc<Mutex<Connection>> = state.clone();
-    let conn2 = conn.lock().unwrap();
+    let conn: Arc<AppState> = state.clone();
+    let conn2 = conn.connection.lock().unwrap();
     conn2.execute(query).unwrap();
     Redirect::to(&("/note?title=".to_owned() + &params.title))
 }
 
-async fn new_card(
-) -> impl IntoResponse {
+async fn new_card() -> impl IntoResponse {
     let mut tera = Tera::new("templates/**/*").unwrap();
     let mut context = tera::Context::new();
     context.insert("date", &format!("{}", Local::now().format("%m/%d/%Y")));
     Html(tera.render("write-card.html.tera", &context).unwrap())
 }
 
+async fn home() -> impl IntoResponse {
+    let mut tera = Tera::new("templates/**/*").unwrap();
+    let mut context = tera::Context::new();
+    Html(tera.render("home.html.tera", &context).unwrap())
+}
+
 #[tokio::main]
 async fn main() {
+    let store = async_session::MemoryStore::new();
+    let session_layer = SessionLayer::new(store, b"hellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohello");
+
     let connection = sqlite::open(":memory:").unwrap();
 
     let query = "
-    CREATE TABLE IF NOT EXISTS users (title TEXT, content TEXT, date TEXT);
+    CREATE TABLE IF NOT EXISTS notes (title TEXT, content TEXT, date TEXT);
+    CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT);
+    INSERT INTO users VALUES ('tonnu', 'passwd');
 ";
     connection.execute(query).unwrap();
 
     println!("we are now here.");
 
-    let query = "SELECT * FROM users";
+    let query = "SELECT * FROM notes";
 
     let conn = Mutex::new(connection);
 
-    let shared_state = Arc::new(conn);
+    let shared_state = Arc::new(AppState { connection: conn });
 
     let app = Router::new()
         .route("/", get(hello))
         .route("/write-a-card", get(new_card))
         .route("/put-card", get(put_card))
         .route("/note", get(note))
+        .route("/login", get(login))
+        .route("/logout", get(logout))
+        .route("/home", get(home))
+        .layer(session_layer)
         .with_state(shared_state);
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
